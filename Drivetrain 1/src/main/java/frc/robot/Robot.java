5a3c49b2d20cdc8bd7c3e80124e6ad39fcd7df20
YukineSekido <60723395+YukineSekido@users.noreply.github.com>
2020-02-08 09:42:54 -0600

    Add files via upload

---
+++ Drivetrain 1/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+
+  private Spark leftMotor = new Spark(0);
+  private Spark rightMotor = new Spark(1);
+  
+  private Joystick joy1 = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() { 
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(4) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotor.set(left);
+    rightMotor.set(right);
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+}
